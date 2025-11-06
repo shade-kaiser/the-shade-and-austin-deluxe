918b63422496634de28f6c3d0951fe2430f4430b
shade-kaiser <shade.kaiser@hotmail.com>
2025-11-05 20:08:57 -0500

    Init commit

---
+++ src/client/java/com/deluxe/TheShadeAndAustinDeluxeClient.java
@@ -0,0 +1,10 @@
+package com.deluxe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheShadeAndAustinDeluxeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
